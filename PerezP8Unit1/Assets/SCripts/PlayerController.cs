b690889c824127757b8aa7271f33a5ab66ff4a13
Evan <Donoyster@icloud.com>
2025-03-26 09:45:20 -0700

    wd
    wd

--- PerezP8Unit1/Assets/SCripts/PlayerController.cs
+++ PerezP8Unit1/Assets/SCripts/PlayerController.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    private float speed = 5.0f;
+    private float turnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+        void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     
     }
 }
