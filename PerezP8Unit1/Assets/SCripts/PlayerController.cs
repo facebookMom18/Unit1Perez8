38d3fb121bc12a1586cc4931faae1aa42ea0fd9e
Evan <Donoyster@icloud.com>
2025-04-24 15:28:08 -0700

    wedw
    e

--- PerezP8Unit1/Assets/SCripts/PlayerController.cs
+++ PerezP8Unit1/Assets/SCripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 5.0f;
+    private float speed = 15.0f;
     private float turnSpeed;
     private float horizontalInput;
     private float forwardInput;
